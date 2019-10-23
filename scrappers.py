b5e325cad62716668062e038b4ffbf4acd4b89fc
Grzegorz <g.w.mika@gmail.com>
2019-10-23 08:07:16 +0200

    scrapers turned to be a class

--- scrappers.py
+++ scrappers.py
@@ -1,18 +1,7 @@
-import tweepy as tw
+class TwitterMiner:
 
-twitter_keys = {
-    'consumer_key':        'g3OU7SzfpnE2AoPzWzC7My8O9',
-    'consumer_secret':     'kIOJAjk4Zun00BvCNCgK4NCSq2X5suMb88IJ12UcSzQUhVLiie',
-    'access_token_key':    '953693483012902912-fXIsILeot5yu6LLk7BBxUiEfi4DwrQD',
-    'access_token_secret': 'vNhUqPByb1SZQMIVuGwhJX8BaGAkROiEMVQ3N6JyYEB2l'
-}
-
-def authorization(keys):
-    auth = tw.OAuthHandler(keys['consumer_key'], keys['consumer_secret'])
-    auth.set_access_token(keys['access_token_key'], keys['access_token_secret'])
-    api = tw.API(auth, wait_on_rate_limit_notify=True)
-    return api
-
-api = authorization(twitter_keys)
-
-print(api)
+    def __init__(self, keys):
+        self.keys = keys
+        auth = tweepy.OAuthHandler(self.keys['consumer_key'], self.keys['consumer_secret'])
+        auth.set_access_token(self.keys['access_token_key'], self.keys['access_token_secret'])
+        self.api = tweepy.API(auth, wait_on_rate_limit_notify=True)
